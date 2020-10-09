8a3a58d100ea0e2e1ef28ad862b5e52e25710b63
DM94 <danmccartney94@gmail.com>
2020-10-08 21:02:49 -0500

    added instructions and tried for ingredients.

--- script.js
+++ script.js
@@ -72,10 +72,21 @@ $(document).ready(function() {
             }).then(function(response){
                 //here is where we will add the recipe information
                 console.log("We were passed the Id:"+id);
-                $("#foodPic").attr("src", response.meals[0].strMealThumb)
+                $("#foodPic").attr("src", response.meals[0].strMealThumb);
+                $("#recipe-title").text(response.meals[0].strMeal);
+                $("#recipe-instructions").text(response.meals[0].strInstructions);
+                (response.meals[0].ingredientList);
+                $("#recipe-measurements").val(response.meals[0].measurementList);
+                
                 let ingredientList = [response.meals[0].strIngredient1,response.meals[0].strIngredient2, response.meals[0].strIngredient3, response.meals[0].strIngredient4,response.meals[0].strIngredient5, response.meals[0].strIngredient6, response.meals[0].strIngredient7, response.meals[0].strIngredient8, response.meals[0].strIngredient9, response.meals[0].strIngredient10, response.meals[0].strIngredient11, response.meals[0].strIngredient12, response.meals[0].strIngredient13, response.meals[0].strIngredient14, response.meals[0].strIngredient15, response.meals[0].strIngredient16, response.meals[0].strIngredient17, response.meals[0].strIngredient18, response.meals[0].strIngredient19, response.meals[0].strIngredient20];    
                 let measurementList = [response.meals[0].strMeasure1,response.meals[0].strMeasure2, response.meals[0].strMeasure3, response.meals[0].strMeasure4,response.meals[0].strMeasure5, response.meals[0].strMeasure6, response.meals[0].strMeasure7, response.meals[0].strMeasure8, response.meals[0].strMeasure9, response.meals[0].strMeasure10, response.meals[0].strMeasure11, response.meals[0].strMeasure12, response.meals[0].strMeasure13, response.meals[0].strMeasure14, response.meals[0].strMeasure15, response.meals[0].strMeasure16, response.meals[0].strMeasure17, response.meals[0].strMeasure18, response.meals[0].strMeasure19, response.meals[0].strMeasure20];
-
+                var myList = $("#ingredient-list");
+                
+                
+                for (var index = 0; index < 19; index++) {
+                    var ingredient = $("<li>"+measurementList[index]+ " " +  ingredientList[index] + "</li>");
+                    myList.append(ingredient);
+                }
                 
                 console.log(ingredientList);
                 console.log(measurementList);
@@ -84,8 +95,8 @@ $(document).ready(function() {
 
         mainIngredient="";        
         }
-    }});
-
+    });
+    
 
     //starting to use the last fm - aminadab
     function generateMusic(country){
